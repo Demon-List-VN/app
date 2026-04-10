47d523c4f2a6177665086a15348ffbc53d23a430
NamPE286 <nambuihung654@gmail.com>
2026-04-11 01:29:33 +0700

    chore: remove test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:gdvn/main.dart';
 
 void main() {
-  testWidgets('theme selector toggles the app theme', (
+  testWidgets('none', (
     WidgetTester tester,
   ) async {
     await tester.pumpWidget(const ForuiApp());
 
-    expect(find.text('ForUI starter'), findsOneWidget);
-
-    await tester.tap(find.byIcon(Icons.dark_mode));
     await tester.pumpAndSettle();
   });
 }
